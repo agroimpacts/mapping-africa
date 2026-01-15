0e195416cc8300c9bda9a386420e2e6583e9e238
Lyndon Estes <3401881+ldemaz@users.noreply.github.com>
2026-01-15 11:37:58 -0500

    Update Zambia PMTiles URLs and Tanzania zoom level

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ window.MA_CONFIG = {
   pmTilesSources: {
     Congo: 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/congo_2022_v1.pmtiles',
     Zambia: {
-      '2018': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2018_v3.pmtiles',
-      '2019': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2019_v3.pmtiles',
-      '2020': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2020_v3.pmtiles',
-      '2021': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2021_v3.pmtiles',
-      '2022': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2022_v3.pmtiles',
-      '2023': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2023_v3.pmtiles'
+      '2018': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2018_v4.pmtiles',
+      '2019': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2019_v4.pmtiles',
+      '2020': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2020_v4.pmtiles',
+      '2021': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2021_v4.pmtiles',
+      '2022': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2022_v4.pmtiles',
+      '2023': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2023_v4.pmtiles'
     },
     Ghana: 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/ghana_2018_v1.pmtiles'
   },
@@ -20,7 +20,7 @@ window.MA_CONFIG = {
     Congo:    { center: [15.275, 0.181],  zoom: 5 },
     Zambia:   { center: [27.8,  -13.1],   zoom: 5.5 },
     Ghana:    { center: [-1.027, 7.858],  zoom: 6 },
-    Tanzania: { center: [34.8955, -6.373], zoom: 14 }
+    Tanzania: { center: [34.8955, -6.373], zoom: 7 }
   },
 
 };
