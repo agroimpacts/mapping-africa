30e7f7ce7ea66be6e9f84c0722f9132b0c413669
YaoTingYao <leannayao@gmail.com>
2026-01-23 14:10:54 -0500

    Refactor config to use year-based source objects
    Updated pmTilesSources and landcoverSources to use objects keyed by year ({ year: url } format) for Congo, Ghana, and Tanzania, aligning their structure with Zambia.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 window.MA_CONFIG = {
   pmTilesSources: {
-    Congo: 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/congo_2022_v1.pmtiles',
+    Congo: {
+      '2022': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/congo_2022_v1.pmtiles'
+    },
     Zambia: {
       '2018': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2018_v4.pmtiles',
       '2019': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2019_v4.pmtiles',
@@ -10,11 +12,15 @@ window.MA_CONFIG = {
       '2023': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2023_v4.pmtiles',
       '2024': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/zambia_2024_v4.pmtiles'
     },
-    Ghana: 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/ghana_2018_v1.pmtiles'
+    Ghana: {
+      '2018': 'https://mappingafrica.s3.us-west-2.amazonaws.com/croplands/pmtiles/ghana_2018_v1.pmtiles'
+    },
   },
 
   landcoverSources: {
-    Tanzania: "https://mappingafrica.s3.us-west-2.amazonaws.com/landcover/tanzania_2019_cog.tif"
+    Tanzania: {
+      '2019': "https://mappingafrica.s3.us-west-2.amazonaws.com/landcover/tanzania_2019_cog.tif"
+    }
   },
 
   countryViews: {
